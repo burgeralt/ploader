baa7208c84bd108b079623396f93b6f16269177c
burgeralt <122481172+burgeralt@users.noreply.github.com>
2023-04-17 12:43:39 -0700

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/Murturtle/ploader/main/loader.js").then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch("https://raw.githubusercontent.com/burgeralt/ploader/main/loader.js").then(data=>{data.text().then(text=>{eval(text)})});
